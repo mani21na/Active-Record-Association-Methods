b1ba181456a58eabc50af263ced463800b9d12b1
Hanna Lee <mani21na@gmail.com>
2019-12-11 22:39:56 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,18 @@
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+
 require 'bundler/setup'
 Bundler.require
 
-require 'active_record'
-require 'rake'
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+# put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
-DBRegistry[ENV["PLAYLISTER_ENV"]].connect!
-DB = ActiveRecord::Base.connection
-
-if ENV["PLAYLISTER_ENV"] == "test"
-  ActiveRecord::Migration.verbose = false
-end
+require_relative "../artist.rb"
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,20 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
-
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "songs", force: :cascade do |t|
-    t.string  "name"
-    t.integer "artist_id"
-    t.integer "genre_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
